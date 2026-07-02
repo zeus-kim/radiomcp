2af743871aa71924a5a87e6811f33ed703229e02
Zeus <zeus@zeus.kim>
2026-07-02 10:11:26 +0900

    chore: bump version to 1.2.8

--- radiomcp/__init__.py
+++ radiomcp/__init__.py
@@ -13,7 +13,7 @@ try:
     from importlib.metadata import version as _pkg_version
     __version__ = _pkg_version("radiomcp")
 except Exception:
-    __version__ = "1.2.7"
+    __version__ = "1.2.8"
 __author__ = "Zeus"
 
 from .server import main, mcp
